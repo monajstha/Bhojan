5656d2168d2d58c04a2a4b60c81c7833515d8617
monajstha <manojstha700@gmail.com>
2024-08-17 10:05:47 +0545

    fix: fix button active state

--- src/index.js
+++ src/index.js
@@ -3,21 +3,45 @@ import homePageContent from "./HomePage/homePage";
 import menuPageContent from "./MenuPage/menuPage";
 import contactPageContent from "./ContactPage/contactPage";
 
-window.addEventListener("load", () => homePageContent());
-
 const homeBtn = document.getElementById("homeBtn");
+const menuBtn = document.getElementById("menuBtn");
+const contactBtn = document.getElementById("contactBtn");
+const content = document.getElementById("content");
+
+content.addEventListener("click", () => {
+  console.log("object");
+});
+
+const buttons = [homeBtn, menuBtn, contactBtn];
+
+function setActiveButton(activeButton) {
+  buttons.forEach((button) => {
+    if (button === activeButton) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+}
+
 homeBtn.addEventListener("click", () => {
   homePageContent();
+  setActiveButton(homeBtn);
 });
 
-const menuBtn = document.getElementById("menuBtn");
 menuBtn.addEventListener("click", () => {
   menuPageContent();
+  setActiveButton(menuBtn);
 });
 
-const contactBtn = document.getElementById("contactBtn");
 contactBtn.addEventListener("click", () => {
   contactPageContent();
+  setActiveButton(contactBtn);
 });
 
-console.log("Hello from Bhojan");
+// display homePage on window load
+window.addEventListener("load", () => {
+  //   homeBtn.focus();
+  setActiveButton(homeBtn);
+  homePageContent();
+});
